43ea29aa1efec3ef41d7bb5736f6181bce57d52f
Christopher Su <christophersu9@gmail.com>
2014-11-20 19:19:38 -0800

    modules

--- src/main/scala/su/christopher/app/MainServlet.scala
+++ src/main/scala/su/christopher/app/MainServlet.scala
@@ -9,9 +9,5 @@ class MainServlet extends GameStack {
     contentType="text/html"
     jade("client/index", "layout" -> "WEB-INF/layouts/empty.jade", "title" -> "Game")
   }
-
-  get("/assets/:path") {
-    Ok(new java.io.File("src/main/webapp/WEB-INF/templates/views/client/assets/" + params("path")))
-  }
   
 }
