59425677b7ae5d8242c061c73b64a60666937a1e
Christopher Su <christophersu9@gmail.com>
2014-11-20 17:03:04 -0800

    submodule

--- src/main/scala/su/christopher/app/MainServlet.scala
+++ src/main/scala/su/christopher/app/MainServlet.scala
@@ -6,12 +6,8 @@ import scalate.ScalateSupport
 class MainServlet extends GameStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType="text/html"
+    jade("client/index")
   }
   
 }
