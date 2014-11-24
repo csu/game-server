41d8ba8afedc536aad6bddff3376844b9ceab1f0
Christopher Su <christophersu9@gmail.com>
2014-11-23 22:29:37 -0800

    modules

--- src/main/scala/su/christopher/app/MainServlet.scala
+++ src/main/scala/su/christopher/app/MainServlet.scala
@@ -2,12 +2,19 @@ package su.christopher.app
 
 import org.scalatra._
 import scalate.ScalateSupport
+import com.boardgame.game._
 
 class MainServlet extends GameStack {
+
+  var game: Game = _
 
   get("/") {
     contentType="text/html"
     jade("client/index", "layout" -> "WEB-INF/layouts/empty.jade", "title" -> "Game")
+  }
+
+  get("/game") {
+    game = new Game(6)
   }
   
 }
